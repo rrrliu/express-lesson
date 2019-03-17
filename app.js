e8f460263511ed5ad178f443a99f8464949bd230
Richard Liu <richard.y.liu@berkeley.edu>
2019-03-17 04:07:08 -0700

    first commit bois

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.listen(port, () => console.log(`App running on port ${port}`));

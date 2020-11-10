a27b1d4344932a15f702a6f62026b032afb07675
Richard Liu <richard.y.liu@berkeley.edu>
2020-11-09 22:06:44 -0800

    POST done

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const app = express();
 
 const port = 8000;
 
-// NEW CODE BLOCK STARTS HERE
 const reviews = [
   { class: 'cs61b', rating: 9, year: 2020, text: 'fun class' },
   { class: 'ee16a', rating: 5, year: 2019, text: 'difficult but worth' },
   { class: 'espm50ac', rating: 10, year: 2020, text: 'easy and fun' },
   { class: 'cs61b', rating: 4, year: 2018, text: 'not well taught :(' },
 ];
-// NEW CODE ENDS HERE
 
 app.get('/', (request, response) => {
   response.send('Hello, world but from an Express server now! 🤩');
@@ -40,5 +38,26 @@ app.get('/reviews/:classname', (request, response) => {
     })
   );
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/reviews', (request, response) => {
+  if (isValidReview(request.body)) {
+    reviews.push(request.body);
+    response.send(`Successfully added review.`);
+  } else {
+    response.status(400).send('Invalid request body');
+  }
+});
+
+const isValidReview = (review) => {
+  return (
+    typeof review.class === 'string' &&
+    typeof review.rating === 'number' &&
+    typeof review.text === 'string' &&
+    typeof review.year === 'number'
+  );
+};
 
 app.listen(port, () => console.log(`App running on port ${port}`));
